0ad616d177e7c1b948d78b72bdf167cff0f1298d
LeylaY1996 <leylay1996@gmail.com>
2020-12-28 23:34:21 +0300

    fixed page

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,159 +1,86 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
- 
-import { SignUpLink } from '../SignUp';
+
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
-import { PasswordForgetLink } from '../PasswordForget';
-
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-
+import { SignUpLink} from '../SignUp/index'
 const SignInPage = () => (
   <div>
     <h1>SignIn</h1>
     <SignInForm />
+    <SignUpLink />
   </div>
 );
- 
+
 const INITIAL_STATE = {
-  email: '',
-  password: '',
+  email:'',
+  password:'',
   error: null,
-};
- 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    height: '100vh',
-  },
-  image: {
-    backgroundImage: 'url(https://source.unsplash.com/random)',
-  }
-}))
+}; 
+
 class SignInFormBase extends Component {
   constructor(props) {
     super(props);
- 
-    this.state = { ...INITIAL_STATE };
+
+    this.state = { ...INITIAL_STATE};
   }
+ 
   onSubmit = event => {
-    const { email, password } = this.state;
- 
+    const { email,password } = this.state;
+console.log("firebase",this.props)
     this.props.firebase
-      .doSignInWithEmailAndPassword(email, password)
-      .then(() => {
-        this.setState({ ...INITIAL_STATE });
-        this.props.history.push(ROUTES.HOME);
-      })
-      .catch(error => {
-        this.setState({ error });
-      });
- 
-    event.preventDefault();
-  };
- 
+          .doSignInWithEmailAndPassword(email,password)
+          .then(() => {
+            this.setState({ ...INITIAL_STATE });
+            this.props.history.push(ROUTES.HOME);
+          })
+          .catch(error => {
+            this.setState({error});
+          });
+        
+          event.preventDefault();
+  }
+
   onChange = event => {
     this.setState({ [event.target.name]: event.target.value });
-  };
- 
+  }
+
   render() {
-    const { email, password, error } = this.state;
- 
+    const {email,password,error } = this.state;
+  
     const isInvalid = password === '' || email === '';
- 
-    const classes = useStyles;
 
     return (
-      <Grid container component="main" className={classes.root}>
-      <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
+      <form onSubmit={this.onSubmit}>
+        <input name="email"
+            value={email}
+            onChange={this.onChange}
+            type="text"
+            placeholder="Email Address"/>
 
-          <form className={classes.form} onSubmit={this.onSubmit}>
-            <input
-            variant="outlined"
-              name="email"
-              margin="normal"
-              required
-              fullWidth
-              value={email}
-              onChange={this.onChange}
-              type="text"
-              placeholder="Email Address"
-              autoFocus
-            />
-            <input
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              value={password}
-              onChange={this.onChange}
-              type="password"
-              placeholder="Password"
-              autoComplete="current-password"
-            />
-            <Button
-             disabled={isInvalid}
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign In
-            </Button>
+        <input name="password"
+            value={password}
+            onChange={this.onChange}
+            type="text"
+            placeholder="Password"/>
 
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                <PasswordForgetLink/>
+        <button disabled={isInvalid} type="submit">
+           Sign In
+        </button>
 
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                <SignUpLink />
-
-                </Link>
-              </Grid>
-            </Grid>
-    
-            {error && <p>{error.message}</p>}
-          </form>
-          </div>
-      </Grid>
-    </Grid>
-    
+        {error && <p>{error.message}</p>}
+      </form>
     );
   }
-}
- 
+  
+};
+
 const SignInForm = compose(
   withRouter,
   withFirebase,
 )(SignInFormBase);
- 
+
 export default SignInPage;
- 
+
 export { SignInForm };
