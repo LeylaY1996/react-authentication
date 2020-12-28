e480ba77d12b4b9d05e708c957bbf847df9513bb
Leylayil <leylay1996@gmail.com>
2020-12-28 11:30:15 +0300

    sign in fixed

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -6,7 +6,21 @@ import { SignUpLink } from '../SignUp';
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
 import { PasswordForgetLink } from '../PasswordForget';
- 
+
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
 const SignInPage = () => (
   <div>
     <h1>SignIn</h1>
@@ -22,13 +36,20 @@ const INITIAL_STATE = {
   error: null,
 };
  
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(https://source.unsplash.com/random)',
+  }
+}))
 class SignInFormBase extends Component {
   constructor(props) {
     super(props);
  
     this.state = { ...INITIAL_STATE };
   }
- 
   onSubmit = event => {
     const { email, password } = this.state;
  
@@ -54,28 +75,34 @@ class SignInFormBase extends Component {
  
     const isInvalid = password === '' || email === '';
  
+    const classes = useStyles;
+
     return (
-      <form onSubmit={this.onSubmit}>
-        <input
-          name="email"
-          value={email}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Email Address"
-        />
-        <input
-          name="password"
-          value={password}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Password"
-        />
-        <button disabled={isInvalid} type="submit">
-          Sign In
-        </button>
- 
-        {error && <p>{error.message}</p>}
-      </form>
+      <Grid container component="main" className={classes.root}>
+        <CssBaseline/>
+        <Grid item xs={false} sm={4} md={7} className={classes.image}/>
+        <form onSubmit={this.onSubmit}>
+          <input
+            name="email"
+            value={email}
+            onChange={this.onChange}
+            type="text"
+            placeholder="Email Address"
+          />
+          <input
+            name="password"
+            value={password}
+            onChange={this.onChange}
+            type="password"
+            placeholder="Password"
+          />
+          <button disabled={isInvalid} type="submit">
+            Sign In
+          </button>
+  
+          {error && <p>{error.message}</p>}
+        </form>
+      </Grid>
     );
   }
 }
